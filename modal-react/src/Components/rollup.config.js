9fd3a095e27f187315207be31f0ddddcaf53dcf2
SNM07 <say.no.more@hotmail.fr>
2022-09-28 14:56:58 +0200

    v3

---
+++ modal-react/src/Components/rollup.config.js
@@ -0,0 +1,21 @@
+import babel from "@rollup/plugin-babel"
+import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+    input: 'components/Modal.jsx',
+    output: {
+      file: 'Modal.js',
+      format: 'cjs'
+    },
+    external: ["react", "styled-components"],
+    plugins: [
+      babel({
+        exclude: "node_module/**",
+          presets: ['@babel/preset-react'],
+          babelHelpers: 'bundled',
+      }),
+      commonjs(),
+      terser(),
+    ]
+};
